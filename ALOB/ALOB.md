d552f850d8d1eb20cc4e2f45a51f088ac33b82ad
Tharnslair <jsmith33@ittesi.com>
2014-09-29 18:29:58 -0400

    on to the view

--- ALOB/ALOB.md
+++ ALOB/ALOB.md
@@ -2,6 +2,22 @@
 ---
 
 ---
+
+##Key factors in building a line of business application
+
+---
+
+Data is a critical asset to your business
+
+Amount of data is significant
+
+Web-friendly tabs
+
+Data integrity is Critical
+
+Data Visualization
+
+
 ---
 
 ___
--- YlixCarRental/YCR.Client.Entities/Properties/AssemblyInfo.cs
+++ YlixCarRental/YCR.Client.Entities/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -35,5 +36,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: ContractNamespace("http://localhost/YlixCarRental",
+[assembly: ContractNamespace("www.whatever.com",
     ClrNamespace = "YCR.Client.Entities")]
